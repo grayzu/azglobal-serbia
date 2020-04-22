2d712a1fc0da4c7204f899fda741198627344dd2
Mark Gray <mark,gray@microsoft.com>
2020-04-21 23:13:12 -0700

    added data source for identity, role assignment to RG, removed subnet

--- main.tf
+++ main.tf
@@ -11,28 +11,22 @@ resource "azurerm_virtual_network" "globalaz_serbia_vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-# Configure Subnet
-resource "azurerm_subnet" "globalaz_serbia_subnet" {
-  name                 = "apps"
-  resource_group_name  = azurerm_resource_group.globalaz_serbia_rg.name
-  virtual_network_name = azurerm_virtual_network.globalaz_serbia_vnet.name
-  address_prefix       = "10.0.1.0/24"
-}
+# # Configure Subnet
+# resource "azurerm_subnet" "globalaz_serbia_subnet" {
+#     name                 = "apps"
+#     resource_group_name  = azurerm_resource_group.globalaz_serbia_rg.name
+#     virtual_network_name = azurerm_virtual_network.globalaz_serbia_vnet.name
+#     address_prefix       = "10.0.1.0/24"
+# }
 
-data "azurerm_user_assigned_identity" "example" {
-  name                = "name_of_user_assigned_identity"
-  resource_group_name = "name_of_resource_group"
+# Give existing identity read permission to resource group
+data "azurerm_user_assigned_identity" "monitor" {
+    name                = "monitor-api"
+    resource_group_name = "globalaz_serbia_mon"
 }
 
 resource "azurerm_role_assignment" "example" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Reader"
-  principal_id         = data.azurerm_client_config.example.object_id
+    scope                = azurerm_resource_group.globalaz_serbia_rg.id
+    role_definition_name = "Reader"
+    principal_id         = data.azurerm_user_assigned_identity.monitor.principal_id
 }
-
-# resource "azurerm_user_assigned_identity" "example" {
-#   resource_group_name = "${azurerm_resource_group.example.name}"
-#   location            = "${azurerm_resource_group.example.location}"
-
-#   name = "search-api"
-# }
